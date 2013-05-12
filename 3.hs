2d70ae540dbbdb83566058d4ea694d3f26c353cd
gadmyth <gadmyth@gmail.com>
2013-05-12 14:28:55 +0800

    add fac func

--- 3.hs
+++ 3.hs
@@ -7,3 +7,10 @@ qsort (x:xs) = qsort smalls ++ [x] ++ qsort larges where
 rev [] = []
 rev (x:xs) = rev xs ++ [x]
 
+
+fac1 0 = 1
+fac1 n = n * fac1 (n - 1)
+
+fac2 n = fac' n 1 where
+     fac' 0 acc = acc
+     fac' n acc = fac' (n - 1) (n * acc)
