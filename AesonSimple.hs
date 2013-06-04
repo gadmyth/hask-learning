3942d26e95f6e609bded1ee32a45051f1f9a09ec
gadmyth <gadmyth@gmail.com>
2013-06-04 14:45:03 +0800

    add aeson json parser to parse json data from web; use Applicative to replace Maybe monad inside operation

--- AesonSimple.hs
+++ AesonSimple.hs
@@ -7,6 +7,13 @@ import Data.Aeson (FromJSON, ToJSON, decode, encode)
 import Data.ByteString.Lazy.Char8
 import GHC.Generics (Generic)
 
+import System.IO.Streams (InputStream)
+import qualified System.IO.Streams as Streams
+import qualified Data.ByteString as S
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Network.Http.Client
+
+import Control.Applicative ((<$>))
 
 data MetaData = MetaData {
      url :: String,
@@ -39,6 +46,20 @@ instance FromJSON Feed
 main :: IO ()
 main = do 
      let req = decode "{\"metadata\":{\"url\":\"FeatureCollection\",\"title\":\"USGS All Earthquakes, Past Hour\"},\"features\":[{\"id\":\"ci15351137\", \"properties\":{\"detail\":\"http://earthquake.usgs.gov/earthquakes/feed/v1.0/detail/ci15351137.geojson\", \"mag\":1.1}}]}" :: Maybe Feed
-     case req of
-          Nothing -> print "parse failed"
-          Just f -> print.("the url of metadata is: "++) $ url $ metadata f
+     print $ ("the url of metadata is: "++) <$> url <$> metadata <$> req
+
+     c <- openConnection "earthquake.usgs.gov" 80
+     q <- buildRequest $ do
+       http GET "/earthquakes/feed/v1.0/summary/all_day.geojson"
+       setAccept "application/json"
+
+     sendRequest c q emptyBody
+     x <- receiveResponse c jsonHandler
+     print $ url <$> metadata <$> x
+
+
+jsonHandler :: Response -> InputStream S.ByteString -> IO (Maybe Feed)
+jsonHandler p i = do
+            chunks <- Streams.toList i
+            let feed = decode (BL.fromChunks chunks) :: Maybe Feed
+            return feed
