4562a362c019a1b27a9d579a4db50ef9d83001a6
gadmyth <gadmyth@gmail.com>
2013-06-07 23:56:40 +0000

    add 3 problems and solutions

--- 99_1_10.hs
+++ 99_1_10.hs
@@ -13,3 +13,25 @@ myButLast [] = error "Empty List!"
 myButLast [_] = error "List has only one element!"
 myButLast [x, _] = x
 myButLast (x:xs) = myButLast xs
+
+-- | P3 Find the K'th element of a list. The first element in the list is number 1.
+elementAt :: (Num b, Eq b) => [a] -> b -> a
+elementAt [] _ = error "List is empty!"
+elementAt (x:xs) n
+          | n == 1 = x
+          | otherwise = elementAt xs (n - 1)
+
+-- | P4 Find the number of elements of a list.  
+myLength :: Num b => [a] -> b
+myLength lst = ml' lst 0 where
+         ml' [] cum = cum
+         ml' (_:xs) cum = ml' xs (cum + 1)
+
+-- | P5 Reverse a list.
+myReverse [] = []
+myReverse (x:xs) = myReverse xs ++ [x]
+
+-- | P6 Find out whether a list is a palindrome. A palindrome can be read forward or backward; e.g. (x a m a x).
+isPal [] = True
+isPal [_] = True
+isPal (x:xs) = (x == last xs) && isPal (init xs)
