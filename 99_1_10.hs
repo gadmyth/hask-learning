6b5df38e981c20a984c261ceecf4f9d30f4bf70b
gadmyth <gadmyth@gmail.com>
2013-06-08 22:04:05 +0000

    finish 1-10 of 99 problems

--- 99_1_10.hs
+++ 99_1_10.hs
@@ -35,3 +35,40 @@ myReverse (x:xs) = myReverse xs ++ [x]
 isPal [] = True
 isPal [_] = True
 isPal (x:xs) = (x == last xs) && isPal (init xs)
+
+-- | P7 Flatten a nested list structure. 
+
+data NestedList a = Elem a | List [NestedList a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = flatten x ++ flatten (List xs)
+
+
+-- | P8 liminate consecutive duplicates of list elements.
+compress [] = []
+compress lst = com' lst [] where
+         com' [] cum = cum
+         com' (x:xs) cum | null cum             = com' xs  (cum ++ [x]) 
+                         | (last cum) == x      = com' xs cum
+                         | otherwise            = com' xs (cum ++ [x])
+
+
+-- | P9 Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists.
+pack [] = []
+pack lst = p' lst [] where 
+     p' [] cum = cum
+     p' (x:xs) cum | null cum = p' xs (cum ++ [[x]])
+                   | (last $ last cum) == x = p' xs ((init cum) ++ [(last cum) ++ [x]])
+                   | otherwise = p' xs (cum ++ [[x]])
+
+-- | P10 Run-length encoding of a list.
+--Use the result of problem P09 to implement the so-called run-length encoding data compression method.
+--Consecutive duplicates of elements are encoded as lists (N E) where N is the number of duplicates of the element E.
+encode [] = []
+encode lst = e' lst [] where 
+     e' [] cum = cum
+     e' (x:xs) cum | null cum = e' xs (cum ++ [(1, x)])
+                   | (snd $ last cum) == x = e' xs ((init cum) ++ [((fst $ last cum) + 1, x)])
+                   | otherwise = e' xs (cum ++ [(1, x)])
+
+
