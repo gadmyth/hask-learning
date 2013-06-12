371d7bfa29e96d83b88c6fb4677b4a7c47c3b9c9
gadmyth <gadmyth@gmail.com>
2013-06-12 15:23:13 +0800

    add quickcheck example

---
+++ quickcheck-test.hs
@@ -0,0 +1,25 @@
+-- | https://www.fpcomplete.com/user/pbv/an-introduction-to-quickcheck-testing
+import Test.QuickCheck
+import Data.List
+import Data.Char as Char
+
+prop_revapp :: [Int] -> [Int] -> Bool
+prop_revapp xs ys = reverse (xs ++ ys) ==
+      reverse ys ++ reverse xs
+
+split :: Char -> String -> [String]
+split c [] = []
+slpit c xs = xs' : if null xs'' then [] else split c (tail xs'')
+      where xs' = takeWhile (/=c) xs
+      	    xs'' = dropWhile (/=c) xs
+
+join :: Char -> [String] -> String
+join c = concat . intersperse [c]
+
+--prop_join_split :: Char -> String -> Bool
+--prop_join_split c xs = join c (split c xs) == xs
+
+prop_join_split' xs = forAll (elements xs) $ \c -> 
+		 join c (split c xs) == xs
+
+main = quickCheck prop_join_split'
