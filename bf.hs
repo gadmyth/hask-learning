bc49f1e90dd877b999c733024964ac45f40678ea
gadmyth <gadmyth@gmail.com>
2013-07-16 07:51:48 +0000

    add BF inteppreter

--- bf.hs
+++ bf.hs
@@ -1,5 +1,10 @@
+-- | http://www.youtube.com/watch?v=VvajXPyKuTo&list=PL_xuff3BkASMOzBr0hKVKLuSnU4UIinKx
+
 import Text.Parsec
 import Text.Parsec.String
+import Control.Monad.State
+import qualified Data.IntMap as M
+import Data.Word
 
 data BFInstruction = GoBack | GoForward | Increment | Decrement | Input
 	 			   | Output | Loop [BFInstruction]
@@ -33,11 +38,49 @@ parseInstruction = do
 parseInstructions :: Parser [BFInstruction]
 parseInstructions = many parseInstruction
 
+type BFRunner = StateT (Int, M.IntMap Word8) IO ()
+
+zeroise :: Maybe Word8 -> Word8
+zeroise = maybe 0 id
+
+runInstruction :: BFInstruction -> BFRunner
+runInstruction GoBack = modify (\(h,m) -> (h-1,m))
+runInstruction GoForward = modify (\(h,m)-> (h+1,m))
+runInstruction Increment = do
+  (bfHead, bfMap) <- get
+  let val = zeroise (M.lookup bfHead bfMap)
+  put (bfHead, M.insert bfHead (val + 1) bfMap)
+
+runInstruction Decrement = do
+  (bfHead, bfMap) <- get
+  let val = zeroise (M.lookup bfHead bfMap)
+  put (bfHead, M.insert bfHead (val - 1) bfMap)
+
+runInstruction Input = do
+  (bfHead, bfMap) <- get
+  c <- liftIO getChar
+  put (bfHead, M.insert bfHead (fromIntegral (fromEnum c)) bfMap)
+
+runInstruction Output = do
+  (bfHead, bfMap) <- get
+  let val = zeroise (M.lookup bfHead bfMap)
+  liftIO $ putChar $ toEnum $ fromIntegral val
+
+runInstruction loop@(Loop insns) = do
+  (bfHead, bfMap) <- get
+  let val = zeroise (M.lookup bfHead bfMap)
+  case val of
+    0 -> return ()
+    _ -> runInstructions insns >> runInstruction loop
+  
+
+runInstructions :: [BFInstruction] -> BFRunner
+runInstructions = mapM_ runInstruction
 
 main :: IO ()
 main = do
   cont <- readFile "hello.bf"
   case parse parseInstructions "hello.bf" cont of
     Left e -> print e
-    Right insn -> print insn
+    Right insns -> evalStateT (runInstructions insns) (0, M.empty)
 
